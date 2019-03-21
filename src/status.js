bb2bbec5afe575c083518655da36a20582d09b9a
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2019-03-21 00:22:10 +0000

    Use two aria live regions, and alternate updates between each. Circumvents react DOM update behaviour, where individual text nodes are updated, causing multiple update events hitting the live region (and causing multiple announcements as a result)

--- src/status.js
+++ src/status.js
@@ -16,13 +16,13 @@ export default class Status extends Component {
   };
 
   state = {
-    bump: '\u00A0'
+    bump: false
   }
 
   componentWillReceiveProps ({ queryLength }) {
     const hasChanged = queryLength !== this.props.queryLength
-    if (hasChanged) {
-      this.setState(({ bump }) => ({ bump: bump+'\u00A0' }))
+    if (hasChanged || !hasChanged) {
+      this.setState(({ bump }) => ({ bump: !bump }))
     }
   }
 
@@ -56,10 +56,10 @@ export default class Status extends Component {
       content = tResults(length, contentSelectedOption)
     }
 
-    return <div
+    return (<div><div
+        id='flip'
       aria-atomic='true'
       aria-live='polite'
-      aria-relevant='all'
       role='status'
       style={{
         border: '0',
@@ -74,8 +74,27 @@ export default class Status extends Component {
         width: '1px'
       }}
     >
-      {content}
-      <span>{bump}</span>
+      <span>{bump ? content : ''}</span>
     </div>
+    <div
+        id='flop'
+      aria-atomic='true'
+      aria-live='polite'
+      role='status'
+      style={{
+        border: '0',
+        clip: 'rect(0 0 0 0)',
+        height: '1px',
+        marginBottom: '-1px',
+        marginRight: '-1px',
+        overflow: 'hidden',
+        padding: '0',
+        position: 'absolute',
+        whiteSpace: 'nowrap',
+        width: '1px'
+      }}
+    >
+      <span>{bump ? '' : content}</span>
+    </div></div>)
   }
 }
